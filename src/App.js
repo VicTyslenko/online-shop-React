ac438f058a6feceb4cc20c17c6658e078f5a864e
Victor Tyslenko <vidrums86@gmail.com>
2022-12-12 10:22:39 +0200

    dash

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
 import LoginPage from './Components/AdminDashboard/LoginPage/LoginPage';
+import { Route, Routes } from 'react-router-dom';
 function App() {
   return (
     <div>
-      <LoginPage />
+      <Routes>
+        <Route path='/' />
+        <Route path='/admin' element={
+          <LoginPage />
+
+        } />
+
+      </Routes>
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -12,9 +12,9 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
     <Provider store={store}>
-        <PersistGate loading={ null } persistor={ persistor }>
-            <App />
-        </PersistGate>
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </BrowserRouter>
 );
