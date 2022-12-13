a42e04b45663ccae3ed2ff556c6fc56f7707cfc4
Yaroslav <kucherenko.yaro@gmail.com>
2022-12-13 18:29:41 +0200

    link dashboard

--- src/@editor/app.js
+++ src/@editor/app.js
@@ -2,14 +2,17 @@ import { Route, Routes } from 'react-router-dom';
 import LoginPage from "./containers/LoginPage/LoginPage";
 import DashboardPage from "./containers/DashboardPage/DashboardPage";
 
-function Editor() {
-	return <div>
-		<Routes>
-			<Route path='/admin' element={<LoginPage />} />
-			<Route path='/dashboard/*' element={<DashboardPage />} />
-		</Routes>
 
-	</div>;
+function Editor() {
+	return (
+		<div>
+			<Routes>
+				<Route path='/admin' element={<LoginPage />} />
+				<Route path='/dashboard/*' element={<DashboardPage />} />
+			</Routes>
+
+		</div>
+	);
 }
 
 export default Editor;
