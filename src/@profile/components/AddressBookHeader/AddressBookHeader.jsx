cfa05d53b6909a673e2b376284ee08f5a33c7ec7
Olena Mits <dubovaya.alena@gmail.com>
2023-01-08 20:48:21 +0100

    fix format problems

--- src/@profile/components/AddressBookHeader/AddressBookHeader.jsx
+++ src/@profile/components/AddressBookHeader/AddressBookHeader.jsx
@@ -1,17 +1,17 @@
-import React from 'react'
-import { MenuList, StyledLink } from './StyledAddressBookHeader'
-const AddressBookHeader = () => {
-    return (
-        <>
-            <h1 className="title">My account</h1>
-            <MenuList>
-                <StyledLink to="/profile">My profile</StyledLink>
-                <StyledLink to="/wishlist">My wishlist</StyledLink>
-                <StyledLink to="/purchase">Purchase history</StyledLink>
-                <StyledLink to="/address">Address book</StyledLink>
-            </MenuList>
-        </>
-    )
+import { MenuList, StyledLink } from './StyledAddressBookHeader';
+
+function AddressBookHeader() {
+	return (
+		<>
+			<h1 className="title">My account</h1>
+			<MenuList>
+				<StyledLink to="/profile">My profile</StyledLink>
+				<StyledLink to="/wishlist">My wishlist</StyledLink>
+				<StyledLink to="/purchase">Purchase history</StyledLink>
+				<StyledLink to="/address">Address book</StyledLink>
+			</MenuList>
+		</>
+	);
 }
 
-export default AddressBookHeader
+export default AddressBookHeader;
--- src/@profile/components/Button/Button.jsx
+++ src/@profile/components/Button/Button.jsx
@@ -1,10 +1,10 @@
-import * as React from 'react';
 import Stack from '@mui/material/Stack';
-import { StyledButton } from './StyledButton';
+import StyledButton from './StyledButton';
+
 export default function BasicButtons() {
-    return (
-        <Stack spacing={2} direction="row">
-            <StyledButton variant="contained">ADD NEW ADDRESS</StyledButton>
-        </Stack>
-    );
+	return (
+		<Stack spacing={2} direction="row">
+			<StyledButton variant="contained">ADD NEW ADDRESS</StyledButton>
+		</Stack>
+	);
 }
