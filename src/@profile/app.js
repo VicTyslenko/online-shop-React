d2e2ce5a058fcf9c734d7f67a0757840acdf1c85
Victor Tyslenko <vidrums86@gmail.com>
2023-01-14 23:56:46 +0200

    start

--- src/@profile/app.js
+++ src/@profile/app.js
@@ -1,11 +1,12 @@
 import { Routes, Route } from 'react-router-dom';
 import AddressBook from './containers/AddressBook/AddressBook';
-
+import LoginOverlayPage from '../components/Login_overlay/LoginOverlayPage/LoginOverlayPage';
 function Profile() {
 	return (
 		<div>
 			<Routes>
 				<Route path="/address" element={<AddressBook />} />
+				<Route path='/login' element={<LoginOverlayPage />} />
 			</Routes>
 		</div>
 	);
---
+++ src/components/Login_overlay/LoginOverlayPage/LoginOverlayPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const LoginOverlayPage = () => {
+  return (
+    <div>LoginOverlayPage</div>
+  )
+}
+
+export default LoginOverlayPage
