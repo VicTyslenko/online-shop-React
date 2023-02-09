981b95a458f110d83b3d8f14c4141fa412adc421
Makar Kovynia <mak.kovynia@gmail.com>
2023-02-09 15:10:41 +0200

    fixed

---
+++ client/src/@main/containers/HomeLandingPage/components/Categories/Categories.styles.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+
+export const ContainerWrapper = styled.div `
+background-color: #000;
+padding-bottom: 115px;
+padding-top: 110px
+`
+
+export const CategoryH1 = styled.h1 `
+font-weight: 400;
+font-size: 24px;
+color: #fff;
+margin-bottom: 35px;
+margin-left: 70px
+`
---
+++ client/src/@main/containers/HomeLandingPage/components/NewCollection/NewCollection.styles.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+
+export const ContainerWrapper = styled.div `
+background-color: #000;
+padding-bottom: 110px;
+padding-top: 85px
+`
+export const NewInH1 = styled.h1 `
+font-weight: 400;
+font-size: 24px;
+color: #fff;
+margin-bottom: 35px;
+margin-left: 70px
+`
