e4e1558ac4d71b0a5a0c943797159c56299722ef
Victor Tyslenko <vidrums86@gmail.com>
2023-02-11 17:50:54 +0200

    fixing-styles

--- client/src/@profile/containers/AddressBookHeader/AddressBookHeader.jsx
+++ client/src/@profile/containers/AddressBookHeader/AddressBookHeader.jsx
@@ -1,13 +1,10 @@
-import {  Wrapp,MenuList } from './StyledAddressBookHeader';
+import { MenuList, Title } from './StyledAddressBookHeader';
 import { useState } from 'react';
 import Tab from '@mui/material/Tab';
 import TabContext from '@mui/lab/TabContext';
 import TabList from '@mui/lab/TabList';
 import TabPanel from '@mui/lab/TabPanel';
-
-  
-  
-  
+import { Container } from '@mui/system';
 // function LinkTab(props) {
 // 	return (
 // 		<Tab
@@ -24,35 +21,26 @@ function AddressBookHeader() {
 	const [value, setValue] = useState('1');
 
 	const handleChange = (event, newValue) => {
-	  setValue(newValue);
-	}
+		setValue(newValue);
+	};
 	return (
-		<Wrapp>
-			
-				<TabContext value={value}>
-			 <TabList onChange={handleChange}>
-        		<Tab  label="My profile" value="1"/>
-      	 	    <Tab  label="My wishlist"  value="2"/>
-     	  		<Tab label="Purchase history" value="3" />
-				<Tab  label="Address book" value="4" />
-      		 </TabList>
-		<TabPanel value="1">My profile</TabPanel>
-        <TabPanel value="2">My wishlist</TabPanel>
-        <TabPanel value="3">Purchase history</TabPanel>
-		<TabPanel value="4">Address book</TabPanel>
-			   </TabContext>
-			  
-
-
-
-			{/* <h1 className="title">My account</h1>
-			<MenuList>
-				<StyledLink to="/profile">My profile</StyledLink>
-				<StyledLink to="/wishlist">My wishlist</StyledLink>
-				<StyledLink to="/purchase">Purchase history</StyledLink>
-				<StyledLink to="/address">Address book</StyledLink>
-			</MenuList> */}
-		</Wrapp>
+		<Container maxWidth="lg">
+			<Title>My account</Title>
+			<TabContext value={value}>
+				<MenuList>
+					<TabList onChange={handleChange}>
+						<Tab className="list-item" label="My profile" value="1" />
+						<Tab className="list-item" label="My wishlist" value="2" />
+						<Tab className="list-item" label="Purchase history" value="3" />
+						<Tab className="list-item" label="Address book" value="4" />
+					</TabList>
+				</MenuList>
+				<TabPanel value="1">My profile</TabPanel>
+				<TabPanel value="2">My wishlist</TabPanel>
+				<TabPanel value="3">Purchase history</TabPanel>
+				<TabPanel value="4">Address book</TabPanel>
+			</TabContext>
+		</Container>
 	);
 }
 
--- client/src/@profile/containers/AddressBookHeader/StyledAddressBookHeader.jsx
+++ client/src/@profile/containers/AddressBookHeader/StyledAddressBookHeader.jsx
@@ -4,7 +4,24 @@ import { Link } from 'react-router-dom';
 export const MenuList = styled.ul`
 	display: flex;
 	justify-content: space-around;
+	gap:30px;
 	align-items: center;
+	& .list-item {
+		font-weight: 400;
+		font-size: 24px;
+		line-height: 33px;
+		color: #828080;
+		font-family: 'Open Sans';
+		text-transform: lowercase;
+	}
+`;
+export const Title = styled.h1`
+	font-weight: 700;
+	font-size: 36px;
+	line-height: 36px;
+	font-family: 'Josefin Sans';
+	color: #000000;
+	margin-bottom:50px;
 `;
 export const StyledLink = styled(Link)`
 	text-decoration: none;
@@ -13,10 +30,9 @@ export const StyledLink = styled(Link)`
 	font-size: 24px;
 	line-height: 33px;
 	font-family: 'Open Sans', sans-serif;
-	margin:25px
-
+	margin: 25px;
 `;
-export const Wrapp = styled.div`
+/* export const Wrapp = styled.div`
 	& .title {
 		margin-bottom: 50px;
 		font-weight: 700;
@@ -25,4 +41,4 @@ export const Wrapp = styled.div`
 		font-family: 'Josefin Sans', sans-serif;
 	
 	}
-`;
+`; */
