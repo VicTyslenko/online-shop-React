0d119ec99f1d9a24a98156867493a47d8aaaae30
Yaroslav <kucherenko.yaro@gmail.com>
2023-02-12 23:36:16 +0200

    finish product task

--- client/src/@editor/components/EditProducts/EditProducts.jsx
+++ client/src/@editor/components/EditProducts/EditProducts.jsx
@@ -1,8 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
+import dayjs from 'dayjs';
 import { MainContent, ContentForm, Header, ContainerWrapp, ButtonWrap, InputTitle } from './StyledEditProducts';
-import { Container, TextField, Box, Button } from '@mui/material';
+import { Container, TextField, Box, Button, MenuItem, Select, InputLabel, FormControl } from '@mui/material';
+import { MobileDatePicker } from '@mui/x-date-pickers/MobileDatePicker';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 
 function EditProducts() {
+	const [categories, setCategories] = useState('');
+	const [enabled, setEnabled] = useState('');
+	const [dataPicker, setDataPicker] = React.useState(dayjs('2023-08-18'));
+
+	const handleEnabled = (event) => {
+		setEnabled(event.target.value);
+	};
+
+	const handleChange = (event) => {
+		setCategories(event.target.value);
+	};
+
+	const handleDataPicker = (newValue) => {
+		setDataPicker(newValue);
+	};
+
 	return (
 		<Container maxWidth="lg">
 			<ContainerWrapp>
@@ -10,31 +30,143 @@ function EditProducts() {
 					<Header>Products</Header>
 					<MainContent>
 						<Box component="form" sx={{ width: '100%', p: '24px' }}>
-							<InputTitle>Name</InputTitle>
+							<FormControl size="small" fullWidth sx={{ mb: '20px' }}>
+								<InputLabel id="select-enabled">Enabled</InputLabel>
+								<Select
+									labelId="select-enabled"
+									id="select-enabled"
+									size="small"
+									fullWidth
+									value={enabled}
+									label="Enabled"
+									onChange={handleEnabled}
+								>
+									<MenuItem value="yes">Yes</MenuItem>
+									<MenuItem value="no">no</MenuItem>
+								</Select>
+							</FormControl>
 							<TextField
-								sx={{ mb: '22px' }}
+								sx={{ mb: '20px' }}
 								fullWidth
 								size="small"
 								id="demo-helper-text-misaligned-no-helper"
 								label="Name"
+								type="string"
 							/>
-							<InputTitle>Email</InputTitle>
 							<TextField
-								sx={{ mb: '22px' }}
+								sx={{ mb: '20px' }}
 								fullWidth
 								size="small"
 								id="demo-helper-text-misaligned-no-helper"
-								label="Email"
+								label="Current price:"
+								type="number"
 							/>
-							<InputTitle>Phone Number</InputTitle>
 							<TextField
-								sx={{ mb: '22px' }}
+								sx={{ mb: '20px' }}
 								fullWidth
 								size="small"
 								id="demo-helper-text-misaligned-no-helper"
-								label="Phone Number"
+								label="Previous price:"
+								type="number"
 							/>
-							<InputTitle>Assing Permissions</InputTitle>
+							<FormControl size="small" fullWidth sx={{ mb: '20px' }}>
+								<InputLabel id="demo-select-small">Categories</InputLabel>
+								<Select
+									labelId="demo-select-small"
+									id="demo-select-small"
+									size="small"
+									fullWidth
+									value={categories}
+									label="Categories"
+									onChange={handleChange}
+								>
+									<MenuItem value={10}>Ten</MenuItem>
+									<MenuItem value={20}>Twenty</MenuItem>
+									<MenuItem value={30}>Thirty</MenuItem>
+								</Select>
+							</FormControl>
+							<TextField
+								sx={{ mb: '20px' }}
+								fullWidth
+								size="small"
+								id="demo-helper-text-misaligned-no-helper"
+								label="Image urls"
+								type="string"
+							/>
+							<TextField
+								sx={{ mb: '20px' }}
+								fullWidth
+								size="small"
+								id="demo-helper-text-misaligned-no-helper"
+								label="Quantity:"
+								type="number"
+							/>
+							<TextField
+								sx={{ mb: '20px' }}
+								fullWidth
+								size="small"
+								id="demo-helper-text-misaligned-no-helper"
+								label="Color"
+								type="string"
+							/>
+							<TextField
+								sx={{ mb: '20px' }}
+								fullWidth
+								size="small"
+								id="demo-helper-text-misaligned-no-helper"
+								label="Sizes"
+								type="string"
+							/>
+							<TextField
+								sx={{ mb: '20px' }}
+								fullWidth
+								size="small"
+								id="demo-helper-text-misaligned-no-helper"
+								label="Product url"
+								type="string"
+							/>
+							<TextField
+								sx={{ mb: '20px' }}
+								fullWidth
+								size="small"
+								id="demo-helper-text-misaligned-no-helper"
+								label="Brand"
+								type="string"
+							/>
+							<TextField
+								sx={{ mb: '20px' }}
+								fullWidth
+								size="small"
+								id="demo-helper-text-misaligned-no-helper"
+								label="Manufacturer"
+								type="string"
+							/>
+							<TextField
+								sx={{ mb: '20px' }}
+								fullWidth
+								size="small"
+								id="demo-helper-text-misaligned-no-helper"
+								label="Manufacturer country"
+								type="string"
+							/>
+							<TextField
+								sx={{ mb: '20px' }}
+								fullWidth
+								size="small"
+								id="demo-helper-text-misaligned-no-helper"
+								label="Seller"
+								type="string"
+							/>
+							<LocalizationProvider dateAdapter={AdapterDayjs}>
+								<MobileDatePicker
+									label="Date"
+									size="small"
+									inputFormat="MM/DD/YYYY"
+									value={dataPicker}
+									onChange={handleDataPicker}
+									renderInput={(params) => <TextField {...params} />}
+								/>
+							</LocalizationProvider>
 						</Box>
 					</MainContent>
 					<ButtonWrap>
