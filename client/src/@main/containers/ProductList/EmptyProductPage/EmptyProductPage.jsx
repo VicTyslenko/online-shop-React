2942e8a447ded9fc8efc6b875d1addaa1731fc52
Victor Tyslenko <vidrums86@gmail.com>
2023-02-22 19:54:24 +0000

    empty-page-added

---
+++ client/src/@main/containers/ProductList/EmptyProductPage/EmptyProductPage.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import { Description } from './StyledEmptyProductPage';
+const EmptyProductPage = () => {
+	return <Description>There is no products</Description>;
+};
+
+export default EmptyProductPage;
--- client/src/@main/containers/ProductList/index.js
+++ client/src/@main/containers/ProductList/index.js
@@ -3,17 +3,19 @@ import { Box } from '@mui/material';
 import { useDispatch, useSelector } from 'react-redux';
 import { useLocation } from 'react-router-dom';
 // import PropTypes from 'prop-types';
-
+import { Fragment } from 'react';
+import EmptyProductPage from './EmptyProductPage/EmptyProductPage';
 import ProductCard from './components/ProductCard';
 import ProductFilters from './components/ProductFilters';
 import { StyledContainer, StyledBox, StyledTitle } from './ProductList.styles';
-
+import Container from '@mui/material';
 import { getProductList } from '../../store/actions/productListActions';
 import { selectProductList } from '../../store/selectors/productListSelectors';
 
 function ProductList() {
 	const products = useSelector(selectProductList);
 	const dispatch = useDispatch();
+	const isNotData = products.length === 0;
 	// const test = useLocation();
 	// console.log('asdf', test);
 
@@ -22,22 +24,44 @@ function ProductList() {
 	}, []);
 
 	return (
-		<StyledContainer maxWidth="lg">
-			{/* todo: Title of category - logic from menu and хлібні крихти для повернення на попередню сторінку замість кнопки назад */}
-			<ProductFilters />
-			<Box sx={{ pb: '30px' }}>
-				<StyledTitle variant="title" component="div">
-					Jackets
-				</StyledTitle>
-				<StyledBox>
-					{products.map(({ name, currentPrice, imageUrls, _id, itemNo }) => (
-						<ProductCard key={_id} title={name} price={currentPrice} url={imageUrls[0]} alt={name} id={itemNo} />
-					))}
-				</StyledBox>
-			</Box>
-		</StyledContainer>
+		<Fragment>
+			{!isNotData && (
+				<StyledContainer maxWidth="lg">
+					{/* todo: Title of category - logic from menu and хлібні крихти для повернення на попередню сторінку замість кнопки назад */}
+					<ProductFilters />
+					<Box sx={{ pb: '30px' }}>
+						<StyledTitle variant="title" component="div">
+							Jackets
+						</StyledTitle>
+						<StyledBox>
+							{products.map(({ name, currentPrice, imageUrls, _id, itemNo }) => (
+								<ProductCard
+									key={_id}
+									title={name}
+									price={currentPrice}
+									url={imageUrls[0]}
+									alt={name}
+									id={itemNo}
+								/>
+							))}
+						</StyledBox>
+					</Box>
+				</StyledContainer>
+			)}
+			{isNotData && (
+				<StyledContainer
+					maxWidth="lg"
+					sx={{
+						display: 'flex',
+						justifyContent: 'center',
+					}}
+				>
+					<EmptyProductPage />
+				</StyledContainer>
+			)}
+		</Fragment>
 	);
-};
+}
 
 // ProductList.propTypes = {
 // 	title: PropTypes.string.isRequired,
