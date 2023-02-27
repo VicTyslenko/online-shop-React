5c8479ceccdd23af8ec384cad605e6b8e8274e56
Victor Tyslenko <vidrums86@gmail.com>
2023-02-27 11:51:48 +0000

    inProgress

--- client/src/@main/containers/ShoppingCart/Modal/Modal.jsx
+++ client/src/@main/containers/ShoppingCart/Modal/Modal.jsx
@@ -16,7 +16,7 @@ const style = {
 	p: 4,
 };
 
-export default function PaymentModal() {
+const PaymentModal = () => {
 	// const [open, setOpen] = React.useState(false);
 	// const handleOpen = () => setOpen(true);
 	// const handleClose = () => setOpen(false);
@@ -25,19 +25,18 @@ export default function PaymentModal() {
 		<div>
 			{/* <Button onClick={handleOpen}>Open modal</Button> */}
 			<Modal
-				// open={open}
-				// onClose={handleClose}
-				// aria-labelledby="modal-modal-title"
-				// aria-describedby="modal-modal-description"
+			// open={open}
+			// onClose={handleClose}
+			// aria-labelledby="modal-modal-title"
+			// aria-describedby="modal-modal-description"
 			>
 				<Box sx={style}>
-				
-					<Typography id="modal-modal-description" sx={{ mt: 2,
-                    fontSize:'20px' }}>
+					<Typography id="modal-modal-description" sx={{ mt: 2, fontSize: '20px' }}>
 						Thank you for chosing our shop
 					</Typography>
 				</Box>
 			</Modal>
 		</div>
 	);
-}
+};
+export default PaymentModal
