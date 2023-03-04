711510052bf7ac5cddb0da35611f05dfe5578690
Yaroslav <kucherenko.yaro@gmail.com>
2023-03-04 20:11:53 +0200

    shopping bag finish

--- client/src/components/Header/components/ShoppingBag/StyledShoppingBag.jsx
+++ client/src/components/Header/components/ShoppingBag/StyledShoppingBag.jsx
@@ -1,18 +1,18 @@
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 
-export const WrappContainer = styled.div`
-	width: 360px;
+export const Title = styled.h3`
+	text-align: center;
+	padding-top: 8px;
+	font-size: 22px;
+`;
 
-	& h3 {
-		text-align: center;
-	}
+export const WrappContainer = styled.div`
+	width: 350px;
+	padding-top: 16px;
 `;
 
 export const ButtonShoppingBag = styled(Link)`
-	position: absolute;
-	left: 60px;
-	bottom: 33px;
 	font-size: 15px;
 	font-style: normal;
 	padding: 10px 30px;
@@ -30,4 +30,73 @@ export const ButtonShoppingBag = styled(Link)`
 	&:hover {
 		background: #434342;
 	}
+`;
+
+export const MainContent = styled.div`
+	position: relative;
+`;
+
+export const ContentItem = styled.div`
+	width: 100%;
+	display: flex;
+	position: relative;
+	padding-bottom: 16px;
+	align-items: center;
+	margin-bottom: 16px;
+	border-bottom: 1px solid #c4c4c4;
+`;
+
+export const ImageWrapp = styled.div`
+	height: 80px;
+
+	img {
+		height: 100%;
+	}
+`;
+
+export const Description = styled.div`
+	display: flex;
+	justify-content: center;
+	padding-left: 26px;
+`;
+
+export const NameItem = styled.h3`
+	margin: 0;
+	font-size: 16px;
+	font-family: 'Open Sans';
+	text-transform: uppercase;
+`;
+
+export const Price = styled.p`
+	display: inline-block;
+	padding-top: 6px;
+	font-size: 16px;
+
+	& .price {
+		font-size: 15px;
+		font-weight: 550;
+	}
+`;
+
+export const CloseItemIcon = styled.div`
+	cursor: pointer;
+	position: absolute;
+	bottom: 6px;
+	right: 15px;
+`;
+
+export const FooterContent = styled.div`
+	position: relative;
+	top: 30px;
+	margin-bottom: 60px;
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+	padding: 0 16px;
+	width: 300px;
+`;
+
+export const TotalPrice = styled.div`
+	font-size: 20px;
+	font-weight: 600;
 `;
