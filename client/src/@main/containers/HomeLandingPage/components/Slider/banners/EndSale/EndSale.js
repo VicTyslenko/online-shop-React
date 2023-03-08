63a4796f494f240f597b0accff728ee7af4c0379
Olena Mits <dubovaya.alena@gmail.com>
2023-03-08 11:56:56 +0000

    Product fix

--- client/src/@main/containers/HomeLandingPage/components/Slider/banners/EndSale/EndSale.js
+++ client/src/@main/containers/HomeLandingPage/components/Slider/banners/EndSale/EndSale.js
@@ -1,18 +1,19 @@
 import React from "react";
-import { StyledLink, StyledText, StyledWrapper } from './EndSale.styles'
+import { Link } from "react-router-dom";
+import { StyledDiv, StyledText, StyledWrapper } from './EndSale.styles'
 
 function EndSale( ){
  return (
-    <React.Fragment>
-    <img src="https://cdn.discordapp.com/attachments/1043545360498442271/1064594361121255484/photo-1519027356316-9f99e11d8bac_1.jpg" />
-    <StyledWrapper>
-        <StyledText>end-of-season sale</StyledText>
-        <StyledLink href="/store/man">
-            <span>SH</span>OP NOW
-        </StyledLink>
-    </StyledWrapper>
-    </React.Fragment>
+    <Link to="/store/man">
+		<img src="https://cdn.discordapp.com/attachments/1043545360498442271/1064594361121255484/photo-1519027356316-9f99e11d8bac_1.jpg" />
+		<StyledWrapper>
+			<StyledText>end-of-season sale</StyledText>
+			<StyledDiv>
+				<span>SH</span>OP NOW
+			</StyledDiv>
+		</StyledWrapper>
+    </Link>
  )
 }
 
-export default EndSale
+export default EndSale;
